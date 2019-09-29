c0164f809175e11a863d50f9b66badda72584e8a
nataliaglebova7 <nataliaglebova7@gmail.com>
2019-09-29 11:57:21 +0300

    Update main.js
    доработано по замечаниям
    1) оптимизированы функции
    2) исправлены ссылки на картинки
    3) исправлены массивы комментариев

--- js/main.js
+++ js/main.js
@@ -3,28 +3,31 @@
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-// функция по генерации комментариев
+// функция генерации случайного элемента массива
+var getRandomElement = function (arr) {
+  var j = Math.floor(Math.random() * arr.length);
+  return arr[j];
+};
+// массив типовых комментариев
 var commentsTemplates = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var getRandomComment = function () {
-  return commentsTemplates[getRandomNumber(0, commentsTemplates.length - 1)];
-};
-
-// функция выбора случайного имени комментатора
+// массив имен комментаторов
 var names = ['Александр', 'Cергей', 'Ольга', 'Владимир', 'Елена', 'Татьяна', 'Андрей'];
-var getRandomName = function () {
-  return names[getRandomNumber(0, names.length - 1)];
-};
 
 // создание массива объектов комментариев
 var commentsList = [];
 var PHOTOCARDS_MAX = 25;
-for (var i = 0; i < PHOTOCARDS_MAX; i++) {
+var COMMENTS_NUMBER = 3;
+for (var i = 0; i < PHOTOCARDS_MAX * COMMENTS_NUMBER; i++) {
   commentsList [i] = {
     avatar: 'img/avatar-' + (getRandomNumber(1, 6)) + '.svg',
-    message: getRandomComment() + ' ' + getRandomComment(),
-    name: getRandomName(),
+    message: getRandomElement(commentsTemplates),
+    name: getRandomElement(names),
   };
+}
+// массив комментариев к одному фото
+var onePhotoComments = [];
+for (var n = 0; n < COMMENTS_NUMBER; n++) {
+  onePhotoComments[n] = getRandomElement(commentsList);
 }
 
 // основной массив из 25 JS объектов
@@ -34,20 +37,19 @@ for (var j = 0; j < PHOTOCARDS_MAX; j++) {
     url: 'photos/' + (j + 1) + '.jpg',
     description: 'Some words about photo',
     likes: getRandomNumber(15, 200),
-    comments: commentsList[getRandomNumber(0, commentsTemplates.length - 1)]
+    comments: onePhotoComments
   };
 }
 // обращение к шаблону верстки карточки фото
 var photoCardTemplate = document.querySelector('#picture').content
-    .querySelector('.picture__info');
+    .querySelector('.picture');
 
 // функция для копирования и вставки типовых карточек фото
-
 var renderPhotoCard = function (arr, q) {
   var photoCardElement = photoCardTemplate.cloneNode(true);
-  // photoCardElement.querySelector('.picture__img').textContent = arr[q].url;
+  photoCardElement.querySelector('.picture__img').src = arr[q].url;
   photoCardElement.querySelector('.picture__likes').textContent = arr[q].likes;
-  photoCardElement.querySelector('.picture__comments').textContent = arr[q].comments.message;
+  photoCardElement.querySelector('.picture__comments').textContent = arr[q].comments.length;
   return photoCardElement;
 };
 
