b8acd5786c835c0770e83bae2b7b507c6f3f6cd7
nataliaglebova7 <nataliaglebova7@gmail.com>
2019-10-21 21:45:42 +0300

    добавление загрузки данных

--- js/photo_gallery.js
+++ js/photo_gallery.js
@@ -15,39 +15,60 @@
   };
 
   // копирование шаблона в нужном количестве
-  var fragment = document.createDocumentFragment();
-  for (var k = 0; k < window.generalData.PHOTOCARDS_MAX; k++) {
-    fragment.appendChild(renderPhotoCard(window.photoData.photoCardItems, k));
-  }
-  // вставка фрагмента
   window.setupPhotoCard = document.querySelector('.pictures');
-  window.setupPhotoCard.appendChild(fragment);
-
-
-  // карточка увеличенной фотографии
-  window.bigPhotoCard = document.querySelector('.big-picture');
+  window.createPhotoGallery = function (arr) {
+    var fragment = document.createDocumentFragment();
+    for (var k = 0; k < window.generalData.PHOTOCARDS_MAX; k++) {
+      fragment.appendChild(renderPhotoCard(arr, k));
+      window.setupPhotoCard.appendChild(fragment);
+    }
+  };
+  // window.createPhotoGallery(window.photoData.photoCardItems);
 
   // разметка комментариев
   // обращение к шаблону верстки карточки фото
   var commentsTemplate = document.querySelector('.social__comment');
 
   // функция для копирования и вставки типовых комментариев
-  var renderPhotoСomments = function (c) {
+  /*  var renderPhotoСomments = function (c) {
     var photoСommentElement = commentsTemplate.cloneNode(true);
     photoСommentElement.querySelector('.social__picture').src = 'img/avatar-' + window.generalData.getRandomNumber(1, 6) + '.svg';
     photoСommentElement.querySelector('.social__picture').alt = window.photoData.onePhotoComments[c].name;
     photoСommentElement.querySelector('.social__text').textContent = window.photoData.onePhotoComments[c].message;
     return photoСommentElement;
-  };
+  };*/
+  // функция по генерации фото в галлереи
+  window.socialComments = document.querySelector('.social__comments');
 
-  var fragmentComments = document.createDocumentFragment();
-  for (var c = 0; c < window.generalData.COMMENTS_NUMBER; c++) {
-    fragmentComments.appendChild(renderPhotoСomments(c));
-  }
-  document.querySelector('.social__comments').appendChild(fragmentComments);
+  window.renderPhotoСomments = function (arr, x) {
+    var fragmentComments = document.createDocumentFragment();
+    while (window.socialComments.firstChild) {
+      window.socialComments.removeChild(document.querySelector('.social__comments').firstChild);
+    }
+    var currentArrowElem = arr[x];
+    for (var c = 0; c < window.generalData.COMMENTS_NUMBER; c++) {
+      var photoСommentElement = commentsTemplate.cloneNode(true);
+      photoСommentElement.querySelector('.social__picture').src = currentArrowElem.comments[c].avatar;
+      photoСommentElement.querySelector('.social__picture').alt = currentArrowElem.comments[c].name;
+      var message = currentArrowElem.comments[c].message;
+      photoСommentElement.querySelector('.social__text').textContent = message;
+      fragmentComments.appendChild(photoСommentElement);
+    }
+    document.querySelector('.social__comments').appendChild(fragmentComments);
+  };
 
   // скрытие блока счетчика комментариев и загрузки новых комментариев
   window.bigPhotoCard.querySelector('.social__comment-count').classList.add('visually-hidden');
   window.bigPhotoCard.querySelector('.comments-loader').classList.add('visually-hidden');
+  // функция по отрисовке ошибки загрузки фотографии
+
+  window.errorLoadPhotoGallery = function () {
+    var errorTemplate = document.querySelector('#error').content
+      .querySelector('.error');
+    var errorElement = errorTemplate.cloneNode(true);
+    var fragmentError = document.createDocumentFragment();
+    fragmentError.appendChild(errorElement);
+    window.setupPhotoCard.appendChild(fragmentError);
+  };
 }
 )();
