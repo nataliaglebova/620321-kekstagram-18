1d8c64261c379e772bcf7cc18e3e837e97601d6f
Natalia Glebova <nataliaglebova7@gmail.com>
2019-10-27 13:44:53 +0300

    усранение дребезга

--- js/photogallery_sort.js
+++ js/photogallery_sort.js
@@ -2,6 +2,21 @@
 var popularFilterButton = window.filters.querySelector('#filter-popular');
 var randomFilterButton = window.filters.querySelector('#filter-random');
 var discussedFilterButton = window.filters.querySelector('#filter-discussed');
+var DEBOUNCE_TIME = 500; // ms
+
+// функция debounce для устранения дребезга
+var debounce = function (f) {
+  var lastTimeout = null;
+  return function () {
+    var parameters = arguments;
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(function () {
+      f.apply(null, parameters);
+    }, DEBOUNCE_TIME);
+  };
+};
 
 // функция перемешивания массива
 var shuffle = function (arr) {
@@ -30,21 +45,33 @@ var sortMostCommentedPhoto = function (arr) {
   return arrCopy;
 };
 
-window.sortPhotogallery = function (dataArr) {
-  window.filters.classList.remove('img-filters--inactive');
-
-  popularFilterButton.addEventListener('click', function () {
-    updatePhotoGallery(dataArr);
-  });
-
-  randomFilterButton.addEventListener('click', function () {
-    var shuffleGallery = shuffle(dataArr);
-    updatePhotoGallery(shuffleGallery);
-  });
-
-  discussedFilterButton.addEventListener('click', function () {
-    var discussingPhotos = sortMostCommentedPhoto(dataArr);
-    updatePhotoGallery(discussingPhotos);
-
-  });
+// функция отображения 10 случайных фото на главной странице
+var renderRandomPhoto = function (dataArr) {
+  var shuffleGallery = shuffle(dataArr);
+  updatePhotoGallery(shuffleGallery);
 };
+// функция для сортировки карточек по количеству комментариев
+var sortPhotoByCommets = function (dataArr) {
+  var discussingPhotos = sortMostCommentedPhoto(dataArr);
+  updatePhotoGallery(discussingPhotos);
+};
+// функции для обработчиков
+var onPopularButtonClick = function () {
+  debounce(updatePhotoGallery(window.photoData.photoCardItems));
+};
+
+var onRandomButtonClick = function () {
+  debounce(renderRandomPhoto(window.photoData.photoCardItems));
+};
+
+var onDiscussedButtonClick = function () {
+  debounce(sortPhotoByCommets(window.photoData.photoCardItems));
+};
+
+
+window.sortPhotogallery = function () {
+  window.filters.classList.remove('img-filters--inactive');
+};
+popularFilterButton.addEventListener('click', onPopularButtonClick);
+randomFilterButton.addEventListener('click', onRandomButtonClick);
+discussedFilterButton.addEventListener('click', onDiscussedButtonClick);
